6b998f1515e4a574c8f2cb7ecc0056fdec6928e2
Ossian Rajala <Rikkokiri@users.noreply.github.com>
2024-04-17 23:08:45 +0300

    Initial commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,23 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
+    "mantine",
+    /* "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    "plugin:react-hooks/recommended", */
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
+  // parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
+    "react/react-in-jsx-scope": "off",
+    "import/extensions": "off",
   },
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+// import { Router } from "./Router";
+import { theme } from "./theme";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import "./i18n/config";
@@ -7,6 +11,8 @@ import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <MantineProvider theme={theme}>
+      <RouterProvider router={router} />
+    </MantineProvider>
   </React.StrictMode>,
 );
