8365c74406ba61d827c59a8515cc51abd7ae1012
Ossian Rajala <ossian.rajala@gmail.com>
2024-06-30 20:47:21 +0300

    fix(candidate page): divider color

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,13 +1,13 @@
-import { Badge, Button, Divider, MantineThemeComponents, Modal, Title } from "@mantine/core";
+import { Badge, Button, Divider, MantineThemeComponents, Modal, Radio, Title } from "@mantine/core";
 import badgeClasses from "./Badge.module.css";
 import buttonClasses from "./Button.module.css";
+import dividerClasses from "./Divider.module.css";
 import modalClasses from "./Modal.module.css";
+import radioClasses from "./Radio.module.css";
 import titleClasses from "./Title.module.css";
 
 export const componentStyles: MantineThemeComponents = {
-  Badge: Badge.extend({
-    classNames: badgeClasses,
-  }),
+  Badge: Badge.extend({ classNames: badgeClasses }),
   Button: Button.extend({
     classNames: buttonClasses,
     defaultProps: {
@@ -15,9 +15,8 @@ export const componentStyles: MantineThemeComponents = {
       size: "md",
     },
   }),
-  Divider: Divider.extend({}),
+  Divider: Divider.extend({ classNames: dividerClasses }),
   Modal: Modal.extend({ classNames: modalClasses }),
-  Title: Title.extend({
-    classNames: titleClasses,
-  }),
+  Radio: Radio.extend({ classNames: radioClasses }),
+  Title: Title.extend({ classNames: titleClasses }),
 };
